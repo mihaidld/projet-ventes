e38335445cf614b12144f5f285d237d09586d317
mihaidld <65900962+mihaidld@users.noreply.github.com>
2026-08-31 12:21:49 +0000

    Add pies for product

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ import plotly.express as px
 import pandas as pd
 
 données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
-
-figure = px.pie(données, values='qte', names='region', title='quantité vendue par région')
-
-figure.write_html('ventes-par-region.html')
-
+données['CA']= données['qte'] * données['prix']
+figure_region = px.pie(données, values='qte', names='region', title='quantité vendue par région')
+figure_region.write_html('ventes-par-region.html')
 print('ventes-par-région.html généré avec succès !')
+
+# a. les ventes par produit
+figure_ventes_produit = px.pie(données, values='qte', names='produit', title='quantité vendue par produit')
+figure_ventes_produit.write_html('ventes-par-produit.html')
+print('ventes-par-produit.html généré avec succès !')
+
+# b. le chiffre d'affaires par produit.
+figure_ca_produit = px.pie(données, values='CA', names='produit', title='CA par produit')
+figure_ca_produit.write_html('ca-par-produit.html')
+print('ca-par-produit.html généré avec succès !')
